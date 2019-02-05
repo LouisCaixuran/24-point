551a180c6687a5efd4cc5bf6c4d0062e2d628714
Louis <louiscaixuran@163.com>
2019-02-05 12:49:12 +0800

    improve the code

--- 24.py
+++ 24.py
@@ -1,5 +1,5 @@
 OPERATER={1:"+",2:"-",3:"*",4:"/"}
-
+flag=False
 def  cal(a,b,action):
 	a=float(a)
 	b=float(b)
@@ -19,6 +19,7 @@ def  cal(a,b,action):
 
 def calculate(a,b,c,d):
 	global OPERATER
+	global flag
 	for i in range(1,5):
 		final1=cal(a,b,OPERATER[i])
 		for j in range(1,5):
@@ -31,35 +32,47 @@ def calculate(a,b,c,d):
 				final6=cal(d,final2,OPERATER[k])
 				final8=cal(final7,d,OPERATER[k])
 				final9=cal(d,final7,OPERATER[k])				
-				if final3==24:
+				if final3==24 and flag==False:
 					print "((",a,OPERATER[i],b,")",OPERATER[j],c,")",OPERATER[k],d,"=",final3
-					exit(0)
-				if final5==24:
+					flag=True
+					return 0
+				if final5==24 and flag==False:
 					print "(",a,OPERATER[i],b,")",OPERATER[k],"(",c,OPERATER[j],d,")=",final5
-					exit(0)
-				if final6==24:
+					flag=True
+					return 0
+				if final6==24 and flag==False:
 					print d,OPERATER[k],"((",a,OPERATER[i],b,")",OPERATER[j],c,")=",final6
-					exit(0)
-				if final8==24:
+					flag=True
+					return 0
+				if final8==24 and flag==False:
 					print "(",c,OPERATER[j],"(",a,OPERATER[i],b,"))",OPERATER[k],d,"=",final8
-					exit(0)
-				if final9==24:
+					flag=True
+					return 0
+				if final9==24 and flag==False:
 					print d,OPERATER[k],"(",c,OPERATER[j],"(",a,OPERATER[i],b,"))=",final9
-					exit(0)
+					flag=True
+					return 0
+
 
 
 def main():
-	a,b,c,d=map(int,raw_input("please input four integers:").split(','))
-	INTEGER={0:a,1:b,2:c,3:d}
-	for i in range(1,5):
-		calculate(INTEGER[(i+1)%4],INTEGER[(i+2)%4],INTEGER[(i+3)%4],INTEGER[(i+4)%4])
-		calculate(INTEGER[(i+1)%4],INTEGER[(i+2)%4],INTEGER[(i+4)%4],INTEGER[(i+3)%4])
-		calculate(INTEGER[(i+1)%4],INTEGER[(i+3)%4],INTEGER[(i+2)%4],INTEGER[(i+4)%4])
-		calculate(INTEGER[(i+1)%4],INTEGER[(i+3)%4],INTEGER[(i+4)%4],INTEGER[(i+2)%4])
-		calculate(INTEGER[(i+1)%4],INTEGER[(i+4)%4],INTEGER[(i+3)%4],INTEGER[(i+2)%4])
-		calculate(INTEGER[(i+1)%4],INTEGER[(i+4)%4],INTEGER[(i+2)%4],INTEGER[(i+3)%4])
-
-	print "no answer"
+	global flag
+	isflag=False
+	while isflag==False:
+		flag=False
+		a,b,c,d=map(int,raw_input("please input four integers:").split(','))
+		INTEGER={0:a,1:b,2:c,3:d}
+		if a==0 and b==0 and c==0 and d==0:
+			isflag=True
+		for i in range(1,5):
+			calculate(INTEGER[(i+1)%4],INTEGER[(i+2)%4],INTEGER[(i+3)%4],INTEGER[(i+4)%4])
+			calculate(INTEGER[(i+1)%4],INTEGER[(i+2)%4],INTEGER[(i+4)%4],INTEGER[(i+3)%4])
+			calculate(INTEGER[(i+1)%4],INTEGER[(i+3)%4],INTEGER[(i+2)%4],INTEGER[(i+4)%4])
+			calculate(INTEGER[(i+1)%4],INTEGER[(i+3)%4],INTEGER[(i+4)%4],INTEGER[(i+2)%4])
+			calculate(INTEGER[(i+1)%4],INTEGER[(i+4)%4],INTEGER[(i+3)%4],INTEGER[(i+2)%4])
+			calculate(INTEGER[(i+1)%4],INTEGER[(i+4)%4],INTEGER[(i+2)%4],INTEGER[(i+3)%4])
+		if flag==False:
+			print "no answer"
 
                     
 
